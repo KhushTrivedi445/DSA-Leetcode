417a77115f2ba921d5c90be93e49d095abb19206
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2026-07-05 23:44:26 +0530

    Time: 3 ms (89.93%), Space: 13.2 MB (97.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,15 +1,17 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        i=0
-        j=0
-        while(i<len(nums)):
-            if (nums[i]!=0 and nums[j]==0) or (nums[i]==0 and nums[j]!=0) :
-                nums[i],nums[j]=nums[j],nums[i]
-                j+=1
+        i = 0
+        j = 0
 
-            elif(nums[j]!=0):
-                j+=1
+        while i < len(nums):
 
-            i+=1
+            # If the current element is non-zero,
+            # move it to the j-th position.
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+            # Always move i
+            i += 1
 
         
